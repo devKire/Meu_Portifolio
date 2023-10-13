9fb3307e760f1698b7a345f58fef9ea0336d1166
devKire <erikdossantos2006@outlook.com>
2023-10-13 14:33:53 -0300

    meu portifolio, arrumando barra lataral

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,17 @@ const header = document.querySelector('.e-zine-header');
 
 window.addEventListener('scroll', () => {
   const currentScrollTop = window.scrollY;
+  var sidebarToggleButton = document.querySelector(".sidebar-toggle-button");
+  var sidebar = document.querySelector(".sidebar");
 
   if (currentScrollTop > lastScrollTop) {
-    // O usuário está rolando para baixo, oculte o header
+    // O usuário está rolando para baixo, oculte o header, o slidebar e feche o botão
     header.style.transition = 'top 0.3s ease';
     header.style.top = '-200px';
+    if (sidebar.style.right === "0px" || sidebar.style.right === "") {
+      sidebar.style.right = "-100px"; // Esconder a barra lateral
+      sidebarToggleButton.classList.remove("clicked"); // Remover a classe clicked
+    }
   } else {
     // O usuário está rolando para cima, mostre o header
     header.style.transition = 'top 0.3s ease';
@@ -32,11 +38,10 @@ window.addEventListener('scroll', () => {
 //   }
 // }
 
-// barra lateral esqueda
+// barra lateral direita
 
 function toggleSidebar() {
   var sidebar = document.querySelector(".sidebar");
-  var sidebarToggleButton = document.querySelector(".sidebar-toggle-button");
   var header = document.querySelector(".e-zine-header");
   var sidebarToggleButton = document.querySelector(".sidebar-toggle-button");
 
